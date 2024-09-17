77c4d4b995825672698baebac7687f6386ebc6e0
Hasan Hammoudah <hassun.h98@gmail.com>
2024-09-17 03:52:07 +0300

    NEW UPDATE

--- src/app/user/user-list/user-list.component.spec.ts
+++ src/app/user/user-list/user-list.component.spec.ts
@@ -1,21 +1,46 @@
+import { of } from 'rxjs';
+import { UserService } from './../user.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
+import { By } from '@angular/platform-browser';
+//TODO Read about all
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
+  let userService: UserService;
+  let userServiceSpy: jasmine.Spy;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [UserListComponent]
-    });
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [UserListComponent],
+      providers:[UserService]
+    }).compileComponents();
     fixture = TestBed.createComponent(UserListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    userService = TestBed.inject(UserService);
+    userServiceSpy = spyOn(userService, 'getUsers').and.returnValue(of([
+      {id: 1, name: "Hasan Hammoudah"},
+      {id:2,name:"Ismail Hammoudah"},
+
+    ]));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should retrieve users from the UserService on init', () => {
+    fixture.detectChanges();
+    expect(userServiceSpy).toHaveBeenCalled();
+  })
+
+  it('should retrieve users from the UserService when the refresh button is clicked', () => {
+    fixture.detectChanges();
+    userServiceSpy.calls.reset();
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+    expect(userServiceSpy).toHaveBeenCalled();
+
+  })
 });
